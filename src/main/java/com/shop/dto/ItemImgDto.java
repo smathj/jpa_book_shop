e2bec789430c092b43db0045ce5cba7b4c2065e8
nataekoon <smathj@naver.com>
2022-10-31 22:30:23 +0900

    2022.10.31 ItemImg 엔티티및 작업

---
+++ src/main/java/com/shop/dto/ItemImgDto.java
@@ -0,0 +1,32 @@
+package com.shop.dto;
+
+import com.shop.entity.ItemImg;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+@Getter
+@Setter
+public class ItemImgDto {
+
+    private Long id;
+
+    private String imgName; // 이미지 파일명
+
+    private String oriImgName;  // 원본 이미지 파일명
+
+    private String imgUrl;  // 이미지 조회 경로
+
+    private String repimgYn;    // 대표 이미지 여부
+    
+    
+    // * ---------------------------------------------------------------------------
+
+    private static ModelMapper modelMapper = new ModelMapper();
+    
+    // * 엔티티를 변수명과 타입이 갖은애들을 복사하여 dto로 리턴함
+    public static ItemImgDto of(ItemImg itemImg) {
+        return modelMapper.map(itemImg, ItemImgDto.class);
+    }
+
+}
